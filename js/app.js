1382da2869901af270f715c7d7d3c0ec005a2467
beniciovargas <beniciorvargas@gmail.com>
2020-02-11 16:15:01 -0800

    added logic to move from start page to main game screen

--- js/app.js
+++ js/app.js
@@ -169,14 +169,32 @@ const game = {
     
         board:[],
     },
+    winningCombos: [
+
+        /* horizontal combos */
+        [0,1,2,3],
+        [4,5,6,7],
+        [8,9,10,11],
+        [12,13,14,15]
+
+        /* vertical combos */
+        
+        [0,4,8,12],
+        [1,5,9,13],
+        [2,6,10,14],
+        [3,7,11,15]
+
+        /*diagonal combos*/
+
+        [0,5,10,15],
+        [3,6,9,12]
+    ],
     
     //initialize game
     init: function(){
         console.log('initializing....')
-        let table = document.querySelector('table');
-        table.parentNode.removeChild(table);
-        let aside = document.querySelector('aside')
-        aside.parentNode.removeChild(aside);
+        let table = document.querySelector('table').style.display = "none";
+        let aside = document.querySelector('aside').style.display = "none";
         let welcome = document.getElementById('welcome');
         welcome.innerHTML = "WELCOME!!"
         let instructions = document.getElementById('instructions')
@@ -189,12 +207,28 @@ const game = {
     gameplayLoop: function(){
         //shuffle objects in card array
         game.shuffleCards(game.loteriaCards);
-        game.createPlayerBoard(game.loteriaCards);
+        game.playerBoardSelects(game.loteriaCards);
         game.shuffleCards(game.loteriaCards);
-        game.createCPUBoard(game.loteriaCards);
+        game.cpuBoardSelects(game.loteriaCards);
+        game.createPlayerBoard(game.loteriaCards);
+        game.loadGameBoard(game.loadGameBoard);
+    },
+
+    createPlayerBoard: function(){
         
+
+    }, 
+    
+    loadGameBoard: function(){
+        //load game board
+        let welcomeContainer = document.querySelector('.welcome-container').style.display = "none"
+        document.querySelector('table').style.display = 'flex'
+        document.querySelector('aside').style.display = 'flex'
+    },
         //load game board
         //initialize boards for user and CPU
+
+
         //generate game board for user that they see - randomize photos to table
         //shuffle deck
         //draw cards
@@ -202,8 +236,8 @@ const game = {
         //assess if theres a match for user or CPU
         //put used card into a new array?
         //print loteria when theres a winner
-    },
-    shuffleCards: function(array){
+    
+    shuffleCards: function (array){
         console.log('shuffle is running')
         for (let i = array.length; i>0; i--){
             const j = Math.floor(Math.random()*(i+1));
@@ -211,19 +245,18 @@ const game = {
         }
             return (game.loteriaCards)
     },
-    createPlayerBoard: function(array){
+    playerBoardSelects: function(array){
         //select 16 into an array for game.player.board
-        console.log('creating board for player')
+        console.log('creating array for player board')
         for (let i=0; i<=15; i++){
             game.player.board.push(game.loteriaCards[i])
         }
         console.log(game.player.board)
 
     },
-
-    createCPUBoard: function(array){
+    cpuBoardSelects: function (array){
         //select 16 into an array for game.CPU.board
-        console.log('creating board for CPU')
+        console.log('creating array for cpu board')
         for (let i=0; i<=15; i++){
             game.CPU.board.push(game.loteriaCards[i])
         }
@@ -231,15 +264,9 @@ const game = {
     },
 }
     
-       
-
-    
-
-
 game.init()
 
-//game.shuffleCards(game.loteriaCards);
-//console.log(game.loteriaCards);
+
 
 
 
